af63934a482a7c03bf0967f7ec332d6897717811
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-11-15 17:23:23 +0900

    修正

--- index.php
+++ index.php
@@ -120,12 +120,9 @@
           <p class="date"><?php echo $result_date[$i] ?></p>
           <!-- <p class="date"><?php echo $result_id[$i] ?></p> -->
         </div>
-        <!-- 値を渡すテスト -->
-        <!-- <form  action="edit.php" method="post"> -->
-          <!-- <label><input type="hidden" name="id" value="<?php echo $result_id[$i] ?>"><label> -->
-          <input type="submit" class="button" id="edit" value="edit">
-        <!-- </form> -->
-      </div>
+          <!-- <input type="hidden" name="id" value="<?php echo $result_id[$i] ?>"><label> -->
+          <button class="button" id="edit">edit</button>
+        </div>
     <?php endfor; ?>
   </div>
   <!-- 出力部分終わり -->
