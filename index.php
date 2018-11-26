7d2bbdc220dca658a35072cde2607059ea5785c2
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-11-26 15:01:43 +0900

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
                               $('#result').html(data);
                           });
 
-                 
+                 location.reload(true);
                         
                       
                     });
