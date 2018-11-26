7495e771889edf7081a27cf409fd51fe6a358fac
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-11-26 12:13:01 +0900

    changeボタンを一時的にコメントアウト

--- index.php
+++ index.php
@@ -70,8 +70,8 @@
       </p>
 
         <!-- changeボタンの一時的な代わり-->
-          <button id="change" style="width: 150px;height: 50px;font-size: 30px;">Change!</button>
-          <div id="result"></div>
+          <!-- <button id="change" style="width: 150px;height: 50px;font-size: 30px;">Change!</button>
+          <div id="result"></div> -->
 
 
       <div id="form-main">
