bd091643488f6355fb30e94ee0181a254d8f75c2
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-11-26 14:45:10 +0900

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
                     $('#change').click(function(){
 
 
-                     $when(
+                     $.when(
 
                           // post方式で'catch.php'に送信する。
                           $.post('catch.php',{
