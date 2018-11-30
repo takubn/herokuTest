786b195575e568e6e0ffd84ef5da9867aac80b0d
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-11-30 16:57:56 +0900

    Update index.php

--- index.php
+++ index.php
@@ -185,10 +185,10 @@
                 <!-- 名前の表示部分 -->
                 <div class="display-name">
                   <!-- 5文字で切り捨て、それを変数に格納 -->
-                    <?php $str_name[] = mb_substr($result_name[$i],0,5,"UTF-8");  ?>
+                    <!-- <?php $str_name[] = mb_substr($result_name[$i],0,5,"UTF-8");  ?> -->
                   
                   <!-- 文字列として一行（名前）を出力。idを自動付与する意図-->
-                    <?php echo "<div class=\"white\" maxlength=\"5\" id=\"name$i\">$str_name[$i]</div>"?>
+                    <?php echo "<div class=\"white\" maxlength=\"5\" id=\"name$i\">$result_name[$i]</div>"?>
                 </div>
                 
                 <!-- 投稿内容の表示部分 -->
