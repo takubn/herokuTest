5a1f0a5dbe8b1a9de1d06d25b9cb93f16c00bcf9
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-11-30 16:02:11 +0900

    Update catch.php

--- catch.php
+++ catch.php
@@ -38,7 +38,7 @@
                     $stmt = $db->prepare($sql);
 
                     $stmt->bindParam(':contents', $contents, PDO::PARAM_STR);
-                    $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+                    $stmt->bindParam(':date', $date, PDO::PARAM_TIMESTAMP);
                     $stmt->execute();
 
                     $db = null;
