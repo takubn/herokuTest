3c211836e6e7c1228df617a134805f3250a9c4ae
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-03 19:00:48 +0900

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,14 @@
 
 <!-- DBに接続し、各種データを取得 -->
 <?php
-require_once("conf/dsn.php");
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$dbname = 'heroku_b24bf788d9d54e3;charset=utf8';
+$user = 'b35095427bfc9e';
+$password = '5efb2b8e';
+
+define('DSN',"mysql:host=$host;dbname=$dbname;charset=utf8");
+define('USER',$user);
+define('PASSWORD',$password);
 
       try{
         $db = new PDO(DSN,USER,PASSWORD);
