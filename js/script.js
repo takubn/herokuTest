65e0337bb85d5e4dc164cff88ca1cb51746c22f9
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-03 17:14:32 +0900

    編集・削除メソッドをjsファイルに追加

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 
-        function scr(){
-          var a = document.documentElement;
-          var y = a.scrollHeight - a.clientHeight;
-          window.scroll(0, y);
-        }
+//一番下までスクロール
+function scr(){
+        var a = document.documentElement;
+        var y = a.scrollHeight - a.clientHeight;
+        window.scroll(0, y);
+}
 
 
 
-
-
-//編集ボタンを押すと入力フォームにその値が挿入されるメソッド
+//編集ボタンを押すと入力フォームに値が挿入され、色が変わるメソッド
 function inputColorEdit(name,content){
   
           // 引数で受け取った値(id)を変数に代入
@@ -46,8 +45,8 @@ function inputColorEdit(name,content){
     }
 
 
-    
-    function inputColorDelete(name,content){
+//削除ボタンを押すと入力フォームに値が挿入され、色が変わるメソッド    
+function inputColorDelete(name,content){
   
       // 引数で受け取った値(id)を変数に代入
       var name = name;
@@ -88,6 +87,87 @@ function inputColorEdit(name,content){
       recontent.disabled = "true";
 
 }
+
+
+// 編集メソッド
+
+    function getId(na,con,id){
+            var na = na.id;
+            var con = con.id;
+            var id = id.textContent;
+        console.log(id);
+
+        if(con !==''){
+            
+            $(function(){
+              
+              //ボタンが押されたら、「SEND」ボタンを「CHANGE」ボタンに変更する。
+            $('#button-blue').replaceWith('<div class="changeBy" id="change" >CHANGE</div>')
+            
+            });
+        }
+
+
+        $(function(){
+
+                    //指定の要素がクリックされたら発火。→変数で表現する
+                    $('#change').click(function(){
+
+                          // post方式で'catch.php'に送信する。
+                          $.post('catch.php',{
+                              // [key名]と[value]=自分で設定してよい　でidとコメントの変更内容を取得
+                              id: id,
+                              contents: $('#comment').val()
+                              //postに成功したときにどうするか。→再読み込み
+                          },function(){
+                              location.reload();
+                          });
+                
+                    });
+                  
+                });                
+        }      
+
+
+
+
+
+// 削除メソッド
+
+  function deleteBy(id){
+      console.log(id);
+      var id = id.textContent;
+      console.log(id);
+
+      if(id !==''){       
+          $(function(){
+            
+            //ボタンが押されたら、「SEND」ボタンを「delete」ボタンに変更する。
+          $('#button-blue').replaceWith('<div class="deleteBy" id="delete" >DELETE</div>')
+          
+          });
+      }
+
+      // post方式でidを渡す
+        $(function(){
+
+          //deleteボタンが押されたら、発火
+          $('#delete').click(function(){
+
+
+
+            //postでidを送信
+            $.post('delete.php',{
+              id:id
+            //成功したら、リロードする
+            },function(){
+              location.reload(true);
+            });
+
+          });
+
+        });
+  }
 
 
 
