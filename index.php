3074f3d3f6bd18c8f938d05d6d5a5f444b0bec0c
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-04 14:39:49 +0900

    cssフォルダ移動

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
   <script src="js/jquery-3.3.1.min.js"></script>
   <link rel="shortcut icon" href="img/favicon.ico">
   <title>dc-board</title>
