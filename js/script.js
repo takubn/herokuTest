df6a0ad797e1cee4b814e1b08bf175598416ca96
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-04 11:56:44 +0900

    script.jsのリファクタリング1

--- js/script.js
+++ js/script.js
@@ -1,174 +1,142 @@
 
-//一番下までスクロール
-function scr(){
-        var a = document.documentElement;
-        var y = a.scrollHeight - a.clientHeight;
-        window.scroll(0, y);
+//一番下までスクロールするメソッド
+function scr() {
+  var a = document.documentElement;
+  var y = a.scrollHeight - a.clientHeight;
+  window.scroll(0, y);
 }
 
 
 
-//編集ボタンを押すと入力フォームに値が挿入され、色が変わるメソッド
-function inputColorEdit(name,content){
-  
-          // 引数で受け取った値(id)を変数に代入
-          var name = name;
-          var content = content;
-  
-          // idから文字情報を取得し、変数に代入
-          var nameText = name.textContent;
-          var contentText = content.textContent;
-  
-  
-          // フォームのIDを取得する。(名前部分)
-              var rename = document.getElementById('name');
-  
-              //フォームの初期値に取ってきた文字情報(name部分)を挿入
-  
-              rename.value = nameText;
+//入力フォームの色と値を変更するメソッド（編集）
+function inputColorEdit(name, content) {
 
-              //入力を無効にする。
-                rename.disabled = "true";
+  var name = name;
+  var content = content;
 
-  
-          // フォームのIDを取得する。(コンテンツ部分)
-          var recontent = document.getElementById('comment');
-  
-          //フォームの初期値に取ってきた文字情報（content部分）を挿入
-  
-          recontent.value = contentText;
-  
-          //フォームの背景色を変更する
-          recontent.style.backgroundColor = 'pink';
+  var nameText = name.textContent;
+  var contentText = content.textContent;
 
-  
-    }
+  // フォームのIDを取得する。(name部分)
+  var rename = document.getElementById('name');
+
+  //フォームの初期値に取ってきた文字情報(name部分)を挿入
+  rename.value = nameText;
+
+  //入力を無効にする。
+  rename.disabled = "true";
 
 
-//削除ボタンを押すと入力フォームに値が挿入され、色が変わるメソッド    
-function inputColorDelete(name,content){
-  
-      // 引数で受け取った値(id)を変数に代入
-      var name = name;
-      var content = content;
-      
+  // フォームのIDを取得する。(contents部分)
+  var recontent = document.getElementById('comment');
 
-      // idから文字情報を取得し、変数に代入
-      var nameText = name.textContent;
-      var contentText = content.textContent;
+  recontent.value = contentText;
+
+  recontent.style.backgroundColor = 'pink';
+}
 
 
-      // フォームのIDを取得する。(名前部分)
-          var rename = document.getElementById('name');
 
-            // console.log(rename);
+//入力フォームの色を変更するメソッド（削除）    
+function inputColorDelete(name, content) {
 
-          //フォームの初期値に取ってきた文字情報(name部分)を挿入
-          rename.value = nameText;
+  var name = name;
+  var content = content;
 
-          //フォームの背景色を変更する
-          rename.style.backgroundColor = 'red';
-
-          //入力を無効にする。
-            rename.disabled = "true";
+  var nameText = name.textContent;
+  var contentText = content.textContent;
 
 
-      // フォームのIDを取得する。(コンテンツ部分)
-      var recontent = document.getElementById('comment');
+  // フォームのIDを取得する。(名前部分)
+  var rename = document.getElementById('name');
 
-      //フォームの初期値に取ってきた文字情報（content部分）を挿入
+  //フォームの初期値に取ってきた文字情報(name部分)を挿入
+  rename.value = nameText;
 
-      recontent.value = contentText;
+  rename.style.backgroundColor = 'red';
 
-      //フォームの背景色を変更する
-      recontent.style.backgroundColor = 'red';
+  //入力を無効にする。
+  rename.disabled = "true";
 
-      //入力を無効にする。
-      recontent.disabled = "true";
+  // フォームのIDを取得する。(contents部分)
+  var recontent = document.getElementById('comment');
+
+  recontent.value = contentText;
+
+  recontent.style.backgroundColor = 'red';
+
+  //入力を無効にする。
+  recontent.disabled = "true";
 
 }
 
 
-// 編集メソッド
+// 編集メソッド(ボタン変更とpost送信)
+function getId(name, contents, id) {
+  var name = name.id;
+  var contents = contents.id;
+  var id = id.textContent;
 
-    function getId(na,con,id){
-            var na = na.id;
-            var con = con.id;
-            var id = id.textContent;
-        console.log(id);
+  console.log(id);
 
-        if(con !==''){
-            
-            $(function(){
-              
-              //ボタンが押されたら、「SEND」ボタンを「CHANGE」ボタンに変更する。
-            $('#button-blue').replaceWith('<div class="changeBy" id="change" >CHANGE</div>')
-            
-            });
-        }
+  if (contents !== '') {
 
+    $(function () {
 
-        $(function(){
+      //ボタンが押されたら、「SEND」ボタンを「CHANGE」ボタンに変更する。
+      $('#button-blue').replaceWith('<div class="changeBy" id="change" >CHANGE</div>')
 
-                    //指定の要素がクリックされたら発火。→変数で表現する
-                    $('#change').click(function(){
-
-                          // post方式で'update.php'に送信する。
-                          $.post("../php/update.php",{
-                              // [key名]と[value]=自分で設定してよい　でidとコメントの変更内容を取得
-                              id: id,
-                              contents: $('#comment').val()
-                              //postに成功したときにどうするか。→再読み込み
-                          },function(){
-                              location.reload();
-                          });
-                
-                    });
-                  
-                });                
-        }      
-
-
-
-
-
-// 削除メソッド
-
-  function deleteBy(id){
-      console.log(id);
-      var id = id.textContent;
-      console.log(id);
-
-      if(id !==''){       
-          $(function(){
-            
-            //ボタンが押されたら、「SEND」ボタンを「delete」ボタンに変更する。
-          $('#button-blue').replaceWith('<div class="deleteBy" id="delete" >DELETE</div>')
-          
-          });
-      }
-
-      // post方式でidを渡す
-        $(function(){
-
-          //deleteボタンが押されたら、発火
-          $('#delete').click(function(){
-
-
-
-            //postでidを送信
-            $.post('../php/delete.php',{
-              id:id
-            //成功したら、リロードする
-            },function(){
-              location.reload(true);
-            });
-
-          });
-
-        });
+    });
   }
 
+  $(function () {
+
+    //「CHANGE」ボタンがクリックされたら発火。
+    $('#change').click(function () {
+
+      $.post("../php/update.php", {
+        // [key]と[value]でidとコメントを送信
+        id: id,
+        contents: $('#comment').val()
+        //postに成功したら再読み込み
+      }, function () {
+        location.reload();
+      });
+
+    });
+
+  });
+}
 
 
+
+// 削除メソッド(ボタン変更とpost送信)
+function deleteBy(id) {
+  var id = id.textContent;
  
+  if (id !== '') {
+    $(function () {
+
+      //ボタンが押されたら、「SEND」ボタンを「delete」ボタンに変更する。
+      $('#button-blue').replaceWith('<div class="deleteBy" id="delete" >DELETE</div>')
+
+    });
+  }
+
+  $(function () {
+
+    //「DELETE」ボタンがクリックされたら、発火。
+    $('#delete').click(function () {
+
+      $.post('../php/delete.php', {
+        // [key]と[value]でidを送信
+        id: id
+        //成功したら、リロードする
+      }, function () {
+        location.reload(true);
+      });
+
+    });
+
+  });
+}
