068ee25bb1d6cd170e971ef3c749fcd974daac66
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-04 12:20:29 +0900

    jsメソッド名再変更
    入力フォームの色を変更する

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function scr() {
 
 
 //入力フォームの色と値を変更するメソッド（編集）
-function inputColorEdit(name, content) {
+function changeFormEdit(name, content) {
 
   var name = name;
   var content = content;
@@ -37,7 +37,7 @@ function inputColorEdit(name, content) {
 
 
 //入力フォームの色を変更するメソッド（削除）    
-function inputColorDelete(name, content) {
+function changeFormDelete(name, content) {
 
   var name = name;
   var content = content;
