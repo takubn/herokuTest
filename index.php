ef15cd8133ea5d05995029d862159fdf24079f2c
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-05 13:25:28 +0900

    Update index.php

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ $max = count($bbs['name']);
           <!-- <?php echo "<div class=\"white-char\" id=\"contents$i\">$result_contents[$i]</div> " ?> -->
 
           <!-- テスト -->
-           <!-- <?php echo "<div class=\"white-char\" id=\"contents$i\">$bbs[\'name\'][$i]</div> " ?> -->
+
 
 
           <p class="date"><?php echo $result_date[$i] ?></p>
