80afcd406ff52c6db9cd234743c4ab81197de876
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-05 14:16:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ echo $bbs['name'][9];
           <!-- <?php echo "<div class=\"white-char\" id=\"contents$i\">$result_contents[$i]</div> " ?> -->
 
           <!-- テスト -->
-          <?php echo "<div class=\"white-char\" id=\"contents$i\">$bbs\[\\'name\'][$i]</div> " ?>
+          <?php echo "<div class=\"white-char\" id=\"contents$i\">$bbs\[\\'name\']\[\$i]</div> " ?>
 
 
           <p class="date"><?php echo $result_date[$i] ?></p>
