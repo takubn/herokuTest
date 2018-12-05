67a6e84f31911d3799c4930b71c096966966377d
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-05 16:00:04 +0900

    Update index.php

--- index.php
+++ index.php
@@ -81,6 +81,9 @@ echo $name;
           <!--多次元配列 テスト -->
           <?php echo "<div class=\"white-char\" id=\"contents$i\">$bbs\['name'][$i]</div> " ?>
 
+          <?php echo "<div class=\"white-char\" id=\"contents$i\">$bbs\['name'\]\[$i\]</div> " ?>
+
+
 
           <p class="date"><?php echo $result_date[$i] ?></p>
           <!-- PrimalyKey取得 -->
