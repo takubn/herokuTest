7be90351737a05c971a7340c302e4b2a8a56a5ca
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-06 18:00:22 +0900

    pagenation.php テスト

--- php/pagenation.php
+++ php/pagenation.php
@@ -1,4 +1,6 @@
  <?php
+//DBに接続（DSN設定を読み込み）
+require_once "/app/conf/dsn.php";
 
 //ページ数・取得位置の決定
 if (isset($_GET['page'])) {
@@ -12,11 +14,25 @@ if (isset($_GET['page'])) {
     $initial_position = 0;
 }
 
-//データ数を取得する。
-$whole_number = $db->prepare("SELECT COUNT(*) id FROM bbs");
-$whole_number->execute();
-$whole_number = $whole_number->fetchColumn();
-//小数点以下を切り上げる。
-$paging_number = ceil($whole_number / 10);
+try {
+    $db = new PDO(DSN, USER, PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo $paging_number;
+//SELECTするデータの初期位置を、GETで取得したパラメーターによって変更する。
+    $limit_data = $db->prepare("SELECT id FROM bbs LIMIT 10 OFFSET {$initial_position}");
+
+    $limit_data->execute();
+    $limit_data = $limit_data->fetchAll(PDO::FETCH_ASSOC);
+
+//データ数を取得する。
+    $whole_number = $db->prepare("SELECT COUNT(*) id FROM bbs");
+    $whole_number->execute();
+    $whole_number = $whole_number->fetchColumn();
+//小数点以下を切り上げる。
+    $paging_number = ceil($whole_number / 10);
+
+    echo $paging_number;
+
+} catch (PDOException $e) {
+    die('エラー：' . $e->getMessage());
+}
