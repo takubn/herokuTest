b123253153d57e678f3bf9a87744345ee778f0bc
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-07 12:36:01 +0900

    try catchを試してみる。

--- php/fetch2.php
+++ php/fetch2.php
@@ -4,38 +4,36 @@ session_start();
 
 //DBに接続（DSN設定を読み込み）
 require_once "/app/conf/dsn.php";
-$db = new PDO(DSN, USER, PASSWORD);
+try {
+    $db = new PDO(DSN, USER, PASSWORD);
 
-//sessionでパラメータ受け取る。
-if (isset($_SESSION['page'])) {
-    $page = (int) $_SESSION['page'];
+    if (isset($_SESSION['page'])) {
+        $page = (int) $_SESSION['page'];
 
-    // データ取得の初期位置を設定　：例　3p目なら「20件～」データを取得
-    $initial_position = ($page * 10) - 10;
-} else {
-    $page = 1;
-    $initial_position = 0;
+        // データ取得の初期位置を設定　：例　3p目なら「20件～」データを取得
+        $initial_position = ($page * 10) - 10;
+    } else {
+        $page = 1;
+        $initial_position = 0;
+    }
+
+//更新された順のデータ10件を昇順（更新日時が新しいほうが下にくるように）で取得する
+    $sql = "SELECT * FROM(SELECT * FROM bbs ORDER BY date desc LIMIT {$initial_position}, 10) AS latestdata ORDER BY date asc ";
+
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($results as $results) {
+        //多次元連想配列に格納
+        $bbs['name'][] = $results['name'];
+        $bbs['contents'][] = $results['contents'];
+        $bbs['date'][] = $results['date'];
+        $bbs['id'][] = $results['id'];
+
+    }
+
+} catch (PDOException $e) {
+    die('エラー：' . $e->getMessage());
 }
-
-//--------------サブクエリ無し設定した開始位置からデータを取り出す。
-
-// $sql = "SELECT * FROM bbs ORDER BY date desc LIMIT {$initial_position},10";
-
-$sql = "SELECT * FROM(SELECT * FROM bbs ORDER BY date desc LIMIT {$initial_position}, 10) AS latestdata ORDER BY date asc ";
-
-$stmt = $db->prepare($sql);
-
-$stmt->execute();
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($results as $results) {
-//多次元連想配列に格納
-    $bbs['name'][] = $results['name'];
-    $bbs['contents'][] = $results['contents'];
-    $bbs['date'][] = $results['date'];
-    $bbs['id'][] = $results['id'];
-
-}
-
-//----------------------終わり----------------------------------------
