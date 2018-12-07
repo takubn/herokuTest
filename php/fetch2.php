d5c614595157b58e8e90cdb795a35216f3410ab0
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-07 11:49:55 +0900

    fetch2.phpを追加

---
+++ php/fetch2.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('display_errors', 1);
+session_start();
+
+//DBに接続（DSN設定を読み込み）
+require_once "/app/conf/dsn.php";
+$db = new PDO(DSN, USER, PASSWORD);
+
+//sessionでパラメータ受け取る。
+if (isset($_SESSION['page'])) {
+    $page = (int) $_SESSION['page'];
+
+    // データ取得の初期位置を設定　：例　3p目なら「20件～」データを取得
+    $initial_position = ($page * 10) - 10;
+} else {
+    $page = 1;
+    $initial_position = 0;
+}
+
+//--------------サブクエリ無し設定した開始位置からデータを取り出す。
+
+$sql = "SELECT * FROM bbs ORDER BY date desc LIMIT {$initial_position},10)";
+
+$sql->execute();
+
+$results = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $results) {
+    echo $results['id'], '：';
+    echo $results['name'], '<br>';
+    echo $results['contents'], '<br>';
+}
+
+//----------------------終わり----------------------------------------
