c9d3e4635c5e6dce56a7172ebc33b241484bfc27
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-07 11:17:48 +0900

    ページング機能のテスト実装
    取得したページ数に応じて取得するデータを変更する。行け！！

--- php/fetch.php
+++ php/fetch.php
@@ -5,7 +5,12 @@ session_start();
 //sessionでパラメータ受け取る。
 if (isset($_SESSION['page'])) {
     $page = (int) $_SESSION['page'];
-    echo $page;
+
+    // データ取得の初期位置を設定　：例　3p目なら「20件～」データを取得
+    $initial_position = ($page * 10) - 10;
+} else {
+    $page = 1;
+    $initial_position = 0;
 }
 
 //DBに接続（DSN設定を読み込み）
@@ -16,7 +21,26 @@ try {
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     //----------更新された順のデータ10件を昇順で取得する。----------
-    $sql = 'SELECT * FROM(SELECT * FROM bbs ORDER BY date desc LIMIT 10) AS latestData ORDER BY date asc';
+    // $sql = 'SELECT * FROM(SELECT * FROM bbs ORDER BY date desc LIMIT 10) AS latestData ORDER BY date asc';
+
+    // $stmt = $db->prepare($sql);
+    // $stmt->execute();
+
+    // $db = null;
+
+    // while (true) {
+    //     $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //     //取り出したデータがなければ、ループ処理終了。
+    //     if ($result == false) {
+    //         break;
+    //     }
+
+//----------終わり--------------------------------------------------
+
+    //--------------設定した開始位置からデータを取り出す。-------------
+
+    $sql = 'SELECT * FROM(SELECT * FROM bbs ORDER BY date desc LIMIT 10 OFFSET{$initial_position}) AS latestdata ORDER BY date asc ';
 
     $stmt = $db->prepare($sql);
     $stmt->execute();
@@ -30,6 +54,8 @@ try {
         if ($result == false) {
             break;
         }
+
+//----------------------終わり----------------------------------------
 
         //多次元連想配列に格納
         $bbs['name'][] = $result['name'];
