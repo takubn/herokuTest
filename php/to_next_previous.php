b242237887cc1128c2e0c09c3c480212775cb51c
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-10 16:39:28 +0900

    Update to_next_previous.php

--- php/to_next_previous.php
+++ php/to_next_previous.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
+$page_next = (int) $_SESSION['page'];
+$page_previous = (int) $_SESSION['page'];
+
 if ($_SESSION['page'] === '') {
     $page_next += 1;
     $page_previous += 1;
 }
-
-$page_next = (int) $_SESSION['page'];
-$page_previous = (int) $_SESSION['page'];
 
 //「次」→現在のページ＋1　「前」→現在のページ-1
 $page_next = $page_next + 1;
