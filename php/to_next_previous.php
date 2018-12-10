fd6858ad8b5ce7a7ba16242acbec45b191eceac1
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-10 16:15:24 +0900

    初期値を0にする

--- php/to_next_previous.php
+++ php/to_next_previous.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 
-$page_next = (int) $_SESSION['page'];
-$page_previous = (int) $_SESSION['page'];
-
-if ($page_next === 1) {
-    $page_next += 1;
-}
+if (isset($_SESSION['page'])) {
+    $page_next = (int) $_SESSION['page'];
+    $page_previous = (int) $_SESSION['page'];
+// if ($page_next === 1) {
+    //     $page_next += 1;
+    // }
 
 //「次」→現在のページ＋1　「前」→現在のページ-1
-$page_next = $page_next + 1;
-$page_previous = $page_previous - 1;
+    $page_next = $page_next + 1;
+    $page_previous = $page_previous - 1;
+}
