a4a9cb2e4a5a65069cf1a4be00ddc0c4b2d20a78
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-10 16:08:24 +0900

    テスト

--- php/to_next_previous.php
+++ php/to_next_previous.php
@@ -4,11 +4,10 @@ session_start();
 $page_next = (int) $_SESSION['page'];
 $page_previous = (int) $_SESSION['page'];
 
+if ($page_next === 1) {
+    $page_next += 1;
+}
+
 //「次」→現在のページ＋1　「前」→現在のページ-1
 $page_next = $page_next + 1;
 $page_previous = $page_previous - 1;
-
-//値が入っていない場合、「0」を代入する。
-// if ($page_previous === '') {
-//     $page_previous = 0;
-// }
