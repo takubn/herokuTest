61299f3d7a4354b8b0397bf9cbebb080b5fc8ee0
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-10 13:46:00 +0900

    Update to_next_previous.php

--- php/to_next_previous.php
+++ php/to_next_previous.php
@@ -6,7 +6,7 @@ $page_for_next_previous = (int) $_SESSION['page'];
 $page_next = $page_for_next_previous + 1;
 $page_previous = $page_for_next_previous - 1;
 
-//「before」ボタンは最初のページには表示させない。
+//「previous」ボタンは最初のページには表示させない。
 if ($page_previous === '') {
     $page_previous = 0;
 }
