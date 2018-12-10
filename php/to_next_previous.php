7e587443c101071b011fbee44d820224fd213eae
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-10 14:24:21 +0900

    Update to_next_previous.php

--- php/to_next_previous.php
+++ php/to_next_previous.php
@@ -1,13 +1,16 @@
 <?php
 session_start();
 
-$page_for_next_previous = (int) $_SESSION['page'];
+if (isset($_SESSION['page'])) {
+
+    $page_for_next_previous = (int) $_SESSION['page'];
 
 //「次」→現在のページ＋1　「前」→現在のページ-1
-$page_next = $page_for_next_previous + 1;
-$page_previous = $page_for_next_previous - 1;
+    $page_next = $page_for_next_previous + 1;
+    $page_previous = $page_for_next_previous - 1;
 
 //値が入っていない場合、「0」を代入する。
-if ($page_previous === '') {
-    $page_previous = 0;
+    if ($page_previous === '') {
+        $page_previous = 0;
+    }
 }
