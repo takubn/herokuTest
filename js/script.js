a37e143867870c40e8e5cf6767bd2942750474b4
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-10 18:05:06 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -147,9 +147,15 @@ function ChangeByParam() {
   var pageParam = location.search.substring(1);
 
   if (pageParam) {
-
+    //パラメータから数値だけを切り取る。
     var pagenum = pageParam.match(/\d+$/)[0]
-    console.log(pagenum);
+
+    var paramid = "id=pager".pagenum;
+
+    var param = getElementById('paramid');
+    console.log(param);
+
+
   }
 
 }
