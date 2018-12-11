b10f948e384230e42059e790245275e6ad1c6086
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-11 11:39:46 +0900

    Update index.php

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ $max = count($bbs['name']);
 
 
   <!-- テスト -->
-   <li id="pager1"><a href="?page=1">1</a></li>
+   <!-- <li id="pager1"><a href="?page=1">1</a></li>
     <li id="pager2"><a href="?page=2">2</a></li>
     <li id="pager3"><a href="?page=3">3</a></li>
     <li id="pager4"><a href="?page=4">4</a></li>
@@ -112,7 +112,7 @@ $max = count($bbs['name']);
     <li id="pager6"><a href="?page=6">6</a></li>
     <li id="pager7"><a href="?page=7">7</a></li>
     <li id="pager8"><a href="?page=8">8</a></li>
-    <li id="pager9"><a href="?page=9">9</a></li>
+    <li id="pager9"><a href="?page=9">9</a></li> -->
 
 
   </div>
