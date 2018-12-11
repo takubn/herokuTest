a31da8983b6c7927f11154298f9a6221bd04439b
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-11 16:57:07 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -163,8 +163,8 @@ function changeColorByParam() {
 
 
   //「a」タグの中からパラメータの数値と一致するcssの色を変更
-  $(function () {
-    $("a:contains(" + paramNumber + ")").toggleClass("paramcolor");
-  });
+  // $(function () {
+  //   $("a:contains(" + paramNumber + ")").toggleClass("paramcolor");
+  // });
 
 }
