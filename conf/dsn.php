94a5c460e92b1552ea73ac67f16977fae454a219
takuro <kawaguchi@g.dreamcareer.co.jp>
2018-12-27 15:34:23 +0900

    ignore ファイルを追加

--- conf/dsn.php
+++ conf/dsn.php
@@ -6,6 +6,7 @@ $dbname = 'heroku_b24bf788d9d54e3;charset=utf8';
 $user = 'b35095427bfc9e';
 $password = '5efb2b8e';
 
+//テスト
 define('DSN', "mysql:host=$host;dbname=$dbname;charset=utf8");
 define('USER', $user);
 define('PASSWORD', $password);
